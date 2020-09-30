240e5e15df7d2704c200855c4e1d4a7c0fc43d0d
surajyadav-7999 <72115850+surajyadav-7999@users.noreply.github.com>
2020-09-30 08:04:37 +0530

    Add files via upload
    while _ Loop

---
+++ do_while.c
@@ -0,0 +1,20 @@
+// exti contol loop
+# include <stdio.h>
+int main()
+{
+    int  a=0;
+    do
+    {
+        printf("plese enter the number %d\n",a);
+        a++;
+
+    }
+    while(a<3);
+    {
+        printf("process is done");
+
+    }
+
+    
+
+}
